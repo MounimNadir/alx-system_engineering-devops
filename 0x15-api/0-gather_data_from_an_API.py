acbdf88bd90e9ae7714b1f0ab2f0bce11cffcbda
root <mounimnadir7@gmail.com>
2024-04-22 18:54:59 -0700

    tak 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,28 @@
 #!/usr/bin/python3
-"""Retrieve and display employee's TODO list progress"""
-
+""" module doc """
 import requests
 import sys
 
+
 def main():
-    """Main function to retrieve and display employee's TODO list progress"""
-    # Validate command-line arguments
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    """ def com """
+    id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done = f'{todos}&completed=true'
+    notDone = f'{todos}&completed=false'
+    userData = requests.get(f'{url}{users}').json()
+    Name = userData[0].get("name")
+    todosData = requests.get(f'{url}{todos}').json()
+    todosDone = requests.get(f'{url}{done}').json()
+    doneN = len(todosDone)
+    totalN = len(todosData)
+    print(f'Employee {Name} is done with tasks({doneN}/{totalN}):')
+    for task in todosDone:
+        print("\t "+task.get("title"))
 
-    employee_id = sys.argv[1]
-
-    # Define base URL
-    base_url = 'https://jsonplaceholder.typicode.com/'
-
-    # Fetch user data
-    user_response = requests.get(f'{base_url}users/{employee_id}')
-    user_data = user_response.json()
-    if 'name' not in user_data:
-        print("User not found.")
-        sys.exit(1)
-    employee_name = user_data["name"]
-
-    # Fetch user's todos
-    todos_response = requests.get(f'{base_url}todos?userId={employee_id}')
-    todos_data = todos_response.json()
-
-    # Fetch completed todos
-    completed_response = requests.get(f'{base_url}todos?userId={employee_id}&completed=true')
-    completed_todos = completed_response.json()
-
-    # Calculate progress
-    total_tasks = len(todos_data)
-    completed_tasks = len(completed_todos)
-
-    # Print progress
-    print(f'Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):')
-    for task in completed_todos:
-        print(f'\t{task["title"]}')
 
 if __name__ == "__main__":
     main()
+
